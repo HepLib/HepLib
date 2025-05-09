3369cddd3f223ee2af739e002433d503b179e278
HepLib <f.feng@outlook.com>
2025-05-09 00:52:37 +0000

    Updated By Github Actions With Build 24 of .github/workflows/doxygen.yml For Github Pages

---
+++ search/classes_3.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['dgamma_0',['DGamma',['../classHepLib_1_1DGamma.html',1,'HepLib']]],
+  ['direct_1',['Direct',['../classHepLib_1_1Direct.html',1,'HepLib']]]
+];
---
+++ search/classes_6.js
@@ -0,0 +1,6 @@
+var searchData=
+[
+  ['hcubature_0',['HCubature',['../classHepLib_1_1SD_1_1HCubature.html',1,'HepLib::SD']]],
+  ['hcubaturemp_1',['HCubatureMP',['../classHepLib_1_1SD_1_1HCubatureMP.html',1,'HepLib::SD']]],
+  ['hookejeeves_2',['HookeJeeves',['../classHepLib_1_1SD_1_1HookeJeeves.html',1,'HepLib::SD']]]
+];
